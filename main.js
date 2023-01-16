1c53e01a223ede982e9f0a5e36412d838c012312
carternadain <cnadain@gmail.com>
2023-01-16 09:24:15 -0700

    add js effects to cards update all html issues w/ links make website more user friendly

--- main.js
+++ main.js
@@ -32,3 +32,31 @@ filterBtns.forEach(btn => {
     });
   });
 });
+
+
+// Get the card element
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(function(card) {
+  card.addEventListener("mouseover", function() {
+    card.classList.add("hover");
+  });
+
+  card.addEventListener("mouseout", function() {
+    card.classList.remove("hover");
+  });
+});
+
+// const card = document.querySelector(".card");
+
+// // Add a mouseover event listener to the card
+// card.addEventListener("mouseover", function() {
+//   // Add the "hover" class to the card when the mouse is over it
+//   card.classList.add("hover");
+// });
+
+// // Add a mouseout event listener to the card
+// card.addEventListener("mouseout", function() {
+//   // Remove the "hover" class from the card when the mouse is not over it
+//   card.classList.remove("hover");
+// });
